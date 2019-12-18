1587e6601b1fe8e5c8af1d1ab8113e0446679c7f
Radek Osmulski <rosmulski@gmail.com>
2019-12-18 11:55:14 +0100

    create Python package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spectral_hyperresolution-radekosmulski"
+    version="0.0.1",
+    author="Radek Osmulski",
+    author_email="radek@earthspecies.org",
+    description="Implementation of linear reassignment in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/earthspecies/spectral_hyperresolution",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
