cfd541fab30dbcbe5a395bf8a76f37d8c94f339b
Radek Osmulski <rosmulski@gmail.com>
2019-12-18 11:57:49 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="spectral_hyperresolution-radekosmulski"
+    name="spectral_hyperresolution-radekosmulski",
     version="0.0.1",
     author="Radek Osmulski",
     author_email="radek@earthspecies.org",
